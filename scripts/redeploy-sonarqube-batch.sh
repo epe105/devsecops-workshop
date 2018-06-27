ee3bf497aeba8265d1dc6f45931b3f528d883328
Manny Evangelista <epe105@users.noreply.github.com>
2018-06-27 18:09:56 -0400

    added redeploy sonarqube

--- scripts/redeploy-sonarqube-batch.sh
+++ scripts/redeploy-sonarqube-batch.sh
@@ -4,10 +4,10 @@
 #hostname="https://192.168.42.136:8443"
 hostname="https://master.ocp-naps.redhatgov.io:8443"
 
-
-for i in {1..20}
+for i in {1..2}
 do
   oc login "$hostname" --insecure-skip-tls-verify -u user${i} -p "redhat!@#"
-  ./provision.sh delete
-  echo "Setup user${i}"
+  ./redeploy_sonarqube.sh
+  echo "redeploy sonarqube for user${i}"
+  sleep 20
 done

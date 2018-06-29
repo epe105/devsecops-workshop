2446e18c7d684f5992eab71aacd0039601b53989
Manny Evangelista <epe105@users.noreply.github.com>
2018-06-29 01:09:12 -0400

    updated secret

--- scripts/provision-batch-delete.sh
+++ scripts/provision-batch-delete.sh
@@ -5,7 +5,7 @@
 hostname="https://master.ocp-naps.redhatgov.io:8443"
 
 
-for i in {1..20}
+for i in {1..5}
 do
   oc login "$hostname" --insecure-skip-tls-verify -u user${i} -p "redhat!@#"
   ./provision.sh delete
--- scripts/provision-batch-setup.sh
+++ scripts/provision-batch-setup.sh
@@ -4,7 +4,7 @@
 #hostname="https://192.168.42.136:8443"
 hostname="https://master.ocp-naps.redhatgov.io:8443"
 
-for i in {1..3}
+for i in {1..5}
 do
   oc login "$hostname" --insecure-skip-tls-verify -u user${i} -p "redhat!@#"
   ./provision.sh deploy --deploy-che --ephemeral

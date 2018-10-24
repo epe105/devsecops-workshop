a3f62281aef41069d78e80f068f430f5c355c14e
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-10-24 13:47:35 -0400

    Added init script

--- scripts/provision-batch-setup.sh
+++ scripts/provision-batch-setup.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-hostname=
-password=
-username=
-begin=1
-count=3
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+ SOURCE="$(readlink "$SOURCE")"
+ [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+source "$DIR/provision-batch-init.sh"
 
-if [[ -z "$hostname" ]]; then
- printf "%s\n" "###############################################################################"
- printf "%s\n" "#  MAKE SURE YOU ARE LOGGED IN TO AN OPENSHIFT CLUSTER:                       #"
- printf "%s\n" "#  $ oc login https://your-openshift-cluster:8443                             #"
- printf "%s\n" "###############################################################################"
- exit 1
-fi
+check-hostname
 
 for (( i = $begin; i <= $count; i++ )); do
  oc login "$hostname" --insecure-skip-tls-verify -u "$username${i}" -p "$password${i}"
- ./provision.sh deploy --ephemeral
- sleep 30
+ ./provision.sh deploy --deploy-che --ephemeral
+ sleep "$pause"
 done

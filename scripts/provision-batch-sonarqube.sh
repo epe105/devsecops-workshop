3cf3d842737783e1f17fc79821cd894c3fcfd6f3
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-10-24 13:47:31 -0400

    Added init script

--- scripts/provision-batch-sonarqube.sh
+++ scripts/provision-batch-sonarqube.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-hostname=
-password=
-username=
-begin=1
-count=3
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+ SOURCE="$(readlink "$SOURCE")"
+ [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+source "$DIR/provision-batch-init.sh"
 
-if [[ -z "$hostname" ]]; then
- printf "%s\n" "###############################################################################"
- printf "%s\n" "#  MAKE SURE YOU ARE LOGGED IN TO AN OPENSHIFT CLUSTER:                       #"
- printf "%s\n" "#  $ oc login https://your-openshift-cluster:8443                             #"
- printf "%s\n" "###############################################################################"
- exit 1
-fi
+check-hostname
 
 for (( i = $begin; i <= $count; i++ )); do
  oc login "$hostname" --insecure-skip-tls-verify -u "$username${i}" -p "$password${i}"
  oc delete service sonarqube && oc delete deploymentconfigs sonarqube && oc delete route sonarqube && oc delete imagestreams sonarqube && oc delete pvc sonarqube-data
  oc new-app -f http://bit.ly/openshift-sonarqube-embedded-template --param=SONARQUBE_VERSION=7.0 --param=SONAR_MAX_MEMORY=4Gi
- sleep 20
+ sleep "$pause"
 done

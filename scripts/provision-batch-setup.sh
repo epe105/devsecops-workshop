77748be868d92e0b218ccaf423270e9fc0a1a65e
ecwpz91 <ecwpz91@users.noreply.github.com>
2019-04-09 09:42:20 -0400

    Updated che ConfigMap to use 'pvc-strategy:common' (https://www.eclipse.org/che/docs/che-6/kubernetes-admin-guide.html#storage-strategies-for-che-workspaces)

--- scripts/provision-batch-setup.sh
+++ scripts/provision-batch-setup.sh
@@ -20,4 +20,7 @@ for (( i = $begin; i <= $count; i++ )); do
  CHE_TOKEN="$(oc sa get-token che -n cicd-$username${i})"
  JSON_STRING='{"data":{"openshift-oauth-token":"'"$CHE_TOKEN"'"}}'
  oc patch configmaps che -p $JSON_STRING -n cicd-$username${i}
+ JSON_STRING='{"data":{"pvc-strategy":"common"}}'
+ oc patch configmaps che -p $JSON_STRING -n cicd-$username${i}
+ oc rollout latest dc/che -n cicd-$username${i}
 done

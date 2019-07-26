77ee64d9b758d79e78f5cf9ce6afbc9435e06d79
tosin Akinosho <takinosh@redhat.com>
2019-07-26 11:09:41 -0400

    adding sonaeqube to cicd-template.yaml

--- scripts/provision-batch-sonarqube.sh
+++ scripts/provision-batch-sonarqube.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+function usage() {
+ echo
+ echo "Usage:"
+ echo " $0 [options]"
+ echo " $0 --help"
+ echo "OPTIONS:"
+ echo "   --ephemeral               Deploy demo without persistent storage. Default false"
+ echo
+}
+
+EPHEMERAL=false
+
+case $1 in
+  --ephemeral)
+   EPHEMERAL=true
+   ;;
+ -h|--help)
+  usage
+  exit 0
+  ;;
+  * )
+   echo  "Deploying with persistent storage"
+   break
+    ;;
+esac
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
@@ -13,6 +38,12 @@ check-hostname
 
 for (( i = $begin; i <= $count; i++ )); do
  oc login "$hostname" --insecure-skip-tls-verify -u "$username${i}" -p "$password"
- #oc delete  all --selector app=sonarqube && oc delete pvc sonarqube-data
- oc new-app -f https://raw.githubusercontent.com/tosin2013/sonarqube-openshift-docker/master/sonarqube-postgresql-template.yaml  --param=SONARQUBE_VERSION=7.9.1
+ oc delete  all --selector app=sonarqube
+ oc delete pvc sonarqube-data
+ oc delete pvc postgresql-sonarqube-data
+ if [ "${EPHEMERAL}" == "true" ] ; then
+    oc new-app -f https://raw.githubusercontent.com/tosin2013/sonarqube-openshift-docker/master/sonarqube-template.yaml  --param=SONARQUBE_VERSION=7.9.1
+ else
+     oc new-app -f https://raw.githubusercontent.com/tosin2013/sonarqube-openshift-docker/master/sonarqube-postgresql-template.yaml  --param=SONARQUBE_VERSION=7.9.1
+ fi
 done

f974521282cc3633109a2d1e80c8aa51f0d2fade
ecwpz91 <ecwpz91@users.noreply.github.com>
2019-10-11 21:13:08 -0400

    Initial revision

---
+++ playbooks/roles/install-butterfly/files/oc-web-terminal.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+hostname=https://{{ openshift_public_hostname }}
+password={{ generic_pass }}
+username={{ generic_user }}
+begin={{ generic_count_begin }}
+count={{ generic_count }}
+pause=5
+
+# Configure openshift.
+# if ! oc whoami &>/dev/null; then
+# printf "Type the server URL that you want to log in to, followed by [ENTER]: "
+#
+# read url
+#
+# if [[ -z "$url" ]]; then
+# printf "OpenShift server URL cannot be null. Please try with a valid URL..."
+# exit 1
+# fi
+#
+# oc login "$url"
+#
+# printf "Type cluster administrator username, followed by [ENTER]: "
+#
+# read admin_user
+#
+# printf "Type cluster administrator password, followed by [ENTER]: "
+#
+# read admin_pass
+# fi
+
+for (( i = $begin; i <= $count; i++ )); do
+ oc login "$hostname" --insecure-skip-tls-verify -u $username${i} -p $password
+
+ # Gather username for project suffix
+ PRJ_SUFFIX=${ARG_PROJECT_SUFFIX:-`echo $(oc $ARG_OC_OPS whoami) | sed -e 's/[-@].*//g'`}
+
+ # # Delete user project.
+ # oc delete project ocwt-$PRJ_SUFFIX
+
+ # Create user project.
+ oc new-project ocwt-$PRJ_SUFFIX
+
+ # Deploy web terminal container.
+ oc new-app quay.io/openshiftlabs/workshop-terminal:2.4.0
+
+ # Expose web terminal container.
+ oc expose svc/workshop-terminal
+
+ # Log out of the cluster.
+ oc logout
+done
